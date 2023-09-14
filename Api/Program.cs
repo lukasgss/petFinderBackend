f030882fc2a530697fdc089c2145d8c9779b89ab
Lucas Gomes <lukasgss37@gmail.com>
2023-09-14 18:46:34 -0300

    add swagger authorization

--- Api/Program.cs
+++ Api/Program.cs
@@ -8,9 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.ConfigureSwagger();
 
 builder.Services.ConfigureIdentity();
 

b676b8f15e77b872abaac12ee0b2a9355812ea8e
lukasgss <69154977+lukasgss@users.noreply.github.com>
2024-02-07 20:05:16 -0300

    fix entity framework dependency versions (#40)

--- Application/Services/General/Images/ImageProcessingService.cs
+++ Application/Services/General/Images/ImageProcessingService.cs
@@ -1,43 +1,45 @@
 using Application.Common.Interfaces.General.Images;
+using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Webp;
+using SixLabors.ImageSharp.Processing;
 
 namespace Application.Services.General.Images;
 
 public class ImageProcessingService : IImageProcessingService
 {
-    // This method returns a MemoryStream, so it's important to note that
-    // the caller is responsible for ensuring the correct disposal of the
-    // stream when done with it.
-    public async Task<MemoryStream> CompressImageAsync(Stream imageStream)
-    {
-        MemoryStream outStream = new();
+	// This method returns a MemoryStream, so it's important to note that
+	// the caller is responsible for ensuring the correct disposal of the
+	// stream when done with it.
+	public async Task<MemoryStream> CompressImageAsync(Stream imageStream)
+	{
+		MemoryStream outStream = new();
 
-        using (Image image = await Image.LoadAsync(imageStream))
-        {
-            int originalWidth = image.Width;
-            int originalHeight = image.Height;
+		using (Image image = await Image.LoadAsync(imageStream))
+		{
+			int originalWidth = image.Width;
+			int originalHeight = image.Height;
 
-            double widthRatio = (double)ImageConfigs.MaxWidth / originalWidth;
-            double heightRatio = (double)ImageConfigs.MaxHeight / originalHeight;
-            double ratio = Math.Min(widthRatio, heightRatio);
+			double widthRatio = (double)ImageConfigs.MaxWidth / originalWidth;
+			double heightRatio = (double)ImageConfigs.MaxHeight / originalHeight;
+			double ratio = Math.Min(widthRatio, heightRatio);
 
-            int newWidth = (int)(originalWidth * ratio);
-            int newHeight = (int)(originalHeight * ratio);
+			int newWidth = (int)(originalWidth * ratio);
+			int newHeight = (int)(originalHeight * ratio);
 
-            image.Mutate(img => img.Resize(new ResizeOptions()
-            {
-                Size = new Size(newWidth, newHeight),
-                Mode = ResizeMode.Max
-            }));
+			image.Mutate(img => img.Resize(new ResizeOptions()
+			{
+				Size = new Size(newWidth, newHeight),
+				Mode = ResizeMode.Max
+			}));
 
-            const int imageQuality = 70;
-            await image.SaveAsync(outStream, new WebpEncoder()
-            {
-                Quality = imageQuality
-            });
-        }
+			const int imageQuality = 70;
+			await image.SaveAsync(outStream, new WebpEncoder()
+			{
+				Quality = imageQuality
+			});
+		}
 
-        outStream.Position = 0;
-        return outStream;
-    }
+		outStream.Position = 0;
+		return outStream;
+	}
 }

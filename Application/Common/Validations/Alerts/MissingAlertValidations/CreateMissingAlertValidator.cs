7986bfda11dd539a0c5bc76ac1b2c132752d3b09
lukasgss <69154977+lukasgss@users.noreply.github.com>
2024-03-02 20:45:04 -0300

    fix missing alert validations (#50)

--- Application/Common/Validations/Alerts/MissingAlertValidations/CreateMissingAlertValidator.cs
+++ Application/Common/Validations/Alerts/MissingAlertValidations/CreateMissingAlertValidator.cs
@@ -5,22 +5,26 @@ namespace Application.Common.Validations.Alerts.MissingAlertValidations;
 
 public class CreateMissingAlertValidator : AbstractValidator<CreateMissingAlertRequest>
 {
-    public CreateMissingAlertValidator()
-    {
-        RuleFor(alert => alert.LastSeenLocationLatitude)
-            .NotNull()
-            .WithMessage("Campo de latitude é obrigatório.")
-            .Must(latitude => latitude >= -90 && latitude <= 90)
-            .WithMessage("Campo de latitude deve ser entre -90 e 90.");
+	public CreateMissingAlertValidator()
+	{
+		RuleFor(alert => alert.LastSeenLocationLatitude)
+			.NotNull()
+			.WithMessage("Campo de latitude é obrigatório.")
+			.Must(latitude => latitude >= -90 && latitude <= 90)
+			.WithMessage("Campo de latitude deve ser entre -90 e 90.");
 
-        RuleFor(alert => alert.LastSeenLocationLongitude)
-            .NotNull()
-            .WithMessage("Campo de longitude é obrigatório.")
-            .Must(longitude => longitude >= -180 && longitude <= 180)
-            .WithMessage("Campo de latitude deve ser entre -180 e 180.");
+		RuleFor(alert => alert.LastSeenLocationLongitude)
+			.NotNull()
+			.WithMessage("Campo de longitude é obrigatório.")
+			.Must(longitude => longitude >= -180 && longitude <= 180)
+			.WithMessage("Campo de latitude deve ser entre -180 e 180.");
 
-        RuleFor(alert => alert.PetId)
-            .NotEmpty()
-            .WithMessage("Campo do pet é obrigatório.");
-    }
+		RuleFor(alert => alert.Description)
+			.MaximumLength(500)
+			.WithMessage("Máximo de 500 caracteres permitidos.");
+
+		RuleFor(alert => alert.PetId)
+			.NotEmpty()
+			.WithMessage("Campo do pet é obrigatório.");
+	}
 }
--- Application/Common/Validations/Alerts/MissingAlertValidations/EditMissingAlertValidator.cs
+++ Application/Common/Validations/Alerts/MissingAlertValidations/EditMissingAlertValidator.cs
@@ -5,26 +5,30 @@ namespace Application.Common.Validations.Alerts.MissingAlertValidations;
 
 public class EditMissingAlertValidator : AbstractValidator<EditMissingAlertRequest>
 {
-    public EditMissingAlertValidator()
-    {
-        RuleFor(alert => alert.Id)
-            .NotEmpty()
-            .WithMessage("Campo de id é obrigatório.");
+	public EditMissingAlertValidator()
+	{
+		RuleFor(alert => alert.Id)
+			.NotEmpty()
+			.WithMessage("Campo de id é obrigatório.");
 
-        RuleFor(alert => alert.LastSeenLocationLatitude)
-            .NotNull()
-            .WithMessage("Campo de latitude é obrigatório.")
-            .Must(latitude => latitude >= -90 && latitude <= 90)
-            .WithMessage("Campo de latitude deve ser entre -90 e 90.");
+		RuleFor(alert => alert.LastSeenLocationLatitude)
+			.NotNull()
+			.WithMessage("Campo de latitude é obrigatório.")
+			.Must(latitude => latitude >= -90 && latitude <= 90)
+			.WithMessage("Campo de latitude deve ser entre -90 e 90.");
 
-        RuleFor(alert => alert.LastSeenLocationLongitude)
-            .NotNull()
-            .WithMessage("Campo de longitude é obrigatório.")
-            .Must(longitude => longitude >= -180 && longitude <= 180)
-            .WithMessage("Campo de latitude deve ser entre -180 e 180.");
+		RuleFor(alert => alert.LastSeenLocationLongitude)
+			.NotNull()
+			.WithMessage("Campo de longitude é obrigatório.")
+			.Must(longitude => longitude >= -180 && longitude <= 180)
+			.WithMessage("Campo de latitude deve ser entre -180 e 180.");
 
-        RuleFor(alert => alert.PetId)
-            .NotEmpty()
-            .WithMessage("Campo do pet é obrigatório.");
-    }
+		RuleFor(alert => alert.Description)
+			.MaximumLength(500)
+			.WithMessage("Máximo de 500 caracteres permitidos.");
+
+		RuleFor(alert => alert.PetId)
+			.NotEmpty()
+			.WithMessage("Campo do pet é obrigatório.");
+	}
 }
